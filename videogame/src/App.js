a8b2b08d7165252eab24d1b209c44f8fe2a3cca9
Trog19 <tsrogers8@msn.com>
2022-07-07 15:53:30 -0600

    Created async function to get all games and return to webframe + tested succesfully

--- videogame/src/App.js
+++ videogame/src/App.js
@@ -1,6 +1,21 @@
+import React, {useEffect, useState} from "react";
+import axios from 'axios';
+
+
 
 function App() {
-  return 
+  const [game, setGame] = useState([]);
+  
+  useEffect(()=>{
+    GetAllGames();
+  }, [])
+}
+
+
+async function GetAllGames(){
+  let response = await axios.get('http://localhost:8080/getAll');
+  setGame(response.data);
+  console.log("List of Games ", response.data)
 }
 
 export default App;
