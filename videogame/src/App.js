445ed282317e6ddb0486bf5e94a23515f4cbfd21
Trog19 <tsrogers8@msn.com>
2022-07-08 08:56:41 -0600

    Created DisplayPlatform.jsx in componenets + succesfully imported google bubble chart

--- videogame/src/App.js
+++ videogame/src/App.js
@@ -1,37 +1,46 @@
 import React, {useEffect, useState} from "react";
 import axios from 'axios';
-
-
+import DisplayPlatform from "./Components/DisplayPlatform";
 
 function App() {
-  const [game, setGame] = useState([]);
+  const [videoGames, setVideoGames] = useState([]);
   
   useEffect(()=>{
-    GetAllGames();
+    GetVideoGames();
   }, [])
-}
 
 
-async function GetAllGames(){
+
+async function GetVideoGames(){
   let response = await axios.get('http://localhost:8080/getAll');
-  setGame(response.data);
+  setVideoGames(response.data);
   console.log("List of Games ", response.data)
 }
 
 
-function App() {
+
   const [gameId, setGameId] = useState([]);
   
   useEffect(()=>{
     GetGameId();
   }, [])
-}
-
 
 async function GetGameId(){
   let response = await axios.get('http://localhost:8080/GetId/10');
-  setGame(response.data);
+  setGameId(response.data);
   console.log("Game with ID ", response.data)
+}
+
+
+return (
+  <div>
+    <h1>"Hello World"</h1>
+    <DisplayPlatform videoGames={videoGames}/>
+  </div>
+);
+
+
+
 }
 
 export default App;
